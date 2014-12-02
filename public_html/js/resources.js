f4b75488ccf5c7e1d2101b12bb5f40fbadaca88c
David Kim <hhjj0506@gmail.com>
2014-12-01 19:50:51 -0800

    12/1/14

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -8,7 +8,8 @@ game.resources = [
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "mario", type:"image", src: "data/img/player1.png"},
         {name: "title-screen", type:"image", src: "data/img/title-screen.png"},
-	
+	{name: "slime", type:"image", src: "data/img/slime-spritesheet.png"},
+        
         /* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
