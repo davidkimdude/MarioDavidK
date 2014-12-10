ec6d0548b575e310c6cb69c864504bd8612d05ce
David Kim <hhjj0506@gmail.com>
2014-12-09 20:02:42 -0800

    commit4

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -4,6 +4,8 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
+        
+        //These are the things that you take from the images so you can use it in the game
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "mario", type:"image", src: "data/img/player1.png"},
@@ -21,6 +23,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
+        
+        //These are the maps that you made in the tiled, so you can put your maps to your game.
         {name: "Davidlevel1", type: "tmx", src: "data/map/Davidlevel1.tmx"},
         {name: "Davidlevel2", type: "tmx", src: "data/map/Davidlevel2.tmx"}
 
